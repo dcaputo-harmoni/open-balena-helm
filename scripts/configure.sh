d90388db39c352f553fa78522f013e74bc265a91
David Caputo <dcaputo@harmoni.io>
2022-03-04 12:26:48 -0500

    Forgot to uncomment

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,7 +3,7 @@
 CONFIG_DIR=$(dirname "$0")/../config
 OPENBALENA_DIR=$(dirname "$0")/../open-balena
 
-#$OPENBALENA_DIR/scripts/quickstart "$@"
+$OPENBALENA_DIR/scripts/quickstart "$@"
 
 echo "==> Creating Openbalena Helm configuration..."
 source "${OPENBALENA_DIR}/config/activate";
