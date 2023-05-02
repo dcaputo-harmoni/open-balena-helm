60f7b1fa7c5bfe99e41da349f2dbcdfc20412181
David Caputo <dcaputo@harmoni.io>
2023-05-02 15:11:31 -0400

    Moving db username / password to env vars

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,8 +3,6 @@
 CONFIG_DIR=$(dirname "$0")/../config
 OPENBALENA_DIR=$(dirname "$0")/../open-balena
 
-$OPENBALENA_DIR/scripts/quickstart "$@"
-
 echo "==> Creating Openbalena Helm configuration..."
 source "${OPENBALENA_DIR}/config/activate";
 envsubst < "${CONFIG_DIR}/values.template.yaml" > "${CONFIG_DIR}/values.yaml"
